746982eddc93e3359f01420d58325f55f7cbf2ee
Mikha Thet <social.amais@gmail.com>
2022-07-11 12:43:37 -0300

    feat: first commit

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode></React.StrictMode>
+);
